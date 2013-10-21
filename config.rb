369a0913fc2ac1a30c9bd2eff13724bcd584859c
Brent Snook <brent@fuglylogic.com>
2013-10-22 10:42:09 +1100

    Move navigation helpers into a module.

--- config.rb
+++ config.rb
@@ -69,46 +69,6 @@ configure :build do
   # set :http_prefix, "/Content/images/"
 end
 
-helpers do
+require 'lib/navigation_helpers'
 
-  class NavLink
-    def initialize attributes
-      @name = attributes[:name]
-      @title = attributes[:title]
-      @path = attributes[:path]
-    end
-
-    def title
-      @title || @name.camelize
-    end
-
-    def path
-      @path || @name
-    end
-
-    def url
-     "/#{path}"
-    end
-
-    def css_class current_path
-      css_class = @name
-      area = (current_path.gsub('index.html', '')).split('/').first || ''
-      puts area
-      puts path
-      css_class += ' current' if area == path
-      css_class
-    end
-
-  end
-
-  def nav_links
-    [
-      NavLink.new(:name => '', :title => 'Home' ),
-      NavLink.new(:name => 'about', :title => 'About Nancy'),
-      NavLink.new(:name => 'wedding-dresses', :title => 'Wedding Dresses'),
-      NavLink.new(:name => 'contact' ),
-      NavLink.new(:name => 'press'),
-      NavLink.new(:name => 'friends')
-    ]
-  end
-end
+helpers NavigationHelpers
---
+++ lib/navigation_helpers.rb
@@ -0,0 +1,40 @@
+class NavLink
+  def initialize attributes
+    @name = attributes[:name]
+    @title = attributes[:title]
+    @path = attributes[:path]
+  end
+
+  def title
+    @title || @name.camelize
+  end
+
+  def path
+    @path || @name
+  end
+
+  def url
+   "/#{path}"
+  end
+
+  def css_class current_path
+    css_class = @name
+    area = (current_path.gsub('index.html', '')).split('/').first || ''
+    css_class += ' current' if area == path
+    css_class
+  end
+end
+
+module NavigationHelpers
+
+  def nav_links
+    [
+      NavLink.new(:name => '', :title => 'Home' ),
+      NavLink.new(:name => 'about', :title => 'About Nancy'),
+      NavLink.new(:name => 'wedding-dresses', :title => 'Wedding Dresses'),
+      NavLink.new(:name => 'contact' ),
+      NavLink.new(:name => 'press'),
+      NavLink.new(:name => 'friends')
+    ]
+  end
+end
