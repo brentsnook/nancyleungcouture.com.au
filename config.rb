b36c854164bd4f6de73e2f135815f0b7cf6c39a3
Brent Snook <brent@fuglylogic.com>
2013-10-21 22:15:44 +1100

    Move sub pages into own directory + simplify links.

--- config.rb
+++ config.rb
@@ -70,19 +70,45 @@ configure :build do
 end
 
 helpers do
-  def nav_link_class name
-    css_class = name
-    css_class += ' current' if current_path.split('.').first == name
-    css_class
+
+  class NavLink
+    def initialize attributes
+      @name = attributes[:name]
+      @title = attributes[:title]
+      @path = attributes[:path]
+    end
+
+    def title
+      @title || @name.camelize
+    end
+
+    def path
+      @path || @name
+    end
+
+    def url
+     "/#{path}"
+    end
+
+    def css_class current_path
+      css_class = @name
+      area = (current_path.gsub('index.html', '')).split('/').first || ''
+      puts area
+      puts path
+      css_class += ' current' if area == path
+      css_class
+    end
+
   end
 
   def nav_links
     [
-      ['Home', 'index'],
-      ['About Nancy', 'about'],
-      ['Wedding Dresses', 'wedding-dresses'],
-      ['Contact', 'contact'], ['Press', 'press'],
-      ['Friends', 'friends']
+      NavLink.new(:name => '', :title => 'Home' ),
+      NavLink.new(:name => 'about', :title => 'About Nancy'),
+      NavLink.new(:name => 'wedding-dresses', :title => 'Wedding Dresses'),
+      NavLink.new(:name => 'contact' ),
+      NavLink.new(:name => 'press'),
+      NavLink.new(:name => 'friends')
     ]
   end
 end
