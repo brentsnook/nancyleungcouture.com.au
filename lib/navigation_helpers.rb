6cb30e21ef90fcd4647ad9792a25dae25feeab74
Brent Snook <brent@fuglylogic.com>
2013-10-22 11:27:14 +1100

    Move NavLink class into helper module.

--- lib/navigation_helpers.rb
+++ lib/navigation_helpers.rb
@@ -1,31 +1,31 @@
-class NavLink
-  def initialize attributes
-    @name = attributes[:name]
-    @title = attributes[:title]
-    @path = attributes[:path]
-  end
-
-  def title
-    @title || @name.camelize
-  end
-
-  def path
-    @path || @name
-  end
-
-  def url
-   "/#{path}"
-  end
-
-  def css_class current_path
-    css_class = @name
-    area = (current_path.gsub('index.html', '')).split('/').first || ''
-    css_class += ' current' if area == path
-    css_class
-  end
-end
-
 module NavigationHelpers
+
+  class NavLink
+    def initialize attributes
+      @name = attributes[:name]
+      @title = attributes[:title]
+      @path = attributes[:path]
+    end
+
+    def title
+      @title || @name.camelize
+    end
+
+    def path
+      @path || @name
+    end
+
+    def url
+     "/#{path}"
+    end
+
+    def css_class current_path
+      css_class = @name
+      area = (current_path.gsub('index.html', '')).split('/').first || ''
+      css_class += ' current' if area == path
+      css_class
+    end
+  end
 
   def nav_links
     [
