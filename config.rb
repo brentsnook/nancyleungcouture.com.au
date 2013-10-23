382e74fdccaf6ec80ee2a3c8128f2b975f16c351
Brent Snook <brent@fuglylogic.com>
2013-10-24 09:58:16 +1100

    Tidy up config.

--- config.rb
+++ config.rb
@@ -1,72 +1,11 @@
-###
-# Compass
-###
-
-# Change Compass configuration
-# compass_config do |config|
-#   config.output_style = :compact
-# end
-
-###
-# Page options, layouts, aliases and proxies
-###
-
-# Per-page layout changes:
-#
-# With no layout
-# page "/path/to/file.html", :layout => false
-#
-# With alternative layout
-# page "/path/to/file.html", :layout => :otherlayout
-#
-# A path which all have the same layout
-with_layout :dress do
-  page "/wedding-dresses/*"
-end
-
-# Proxy pages (http://middlemanapp.com/dynamic-pages/)
-# proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
-#  :which_fake_page => "Rendering a fake page with a local variable" }
-
-###
-# Helpers
-###
-
-# Automatic image dimensions on image_tag helper
-# activate :automatic_image_sizes
-
-# Reload the browser automatically whenever files change
-# activate :livereload
-
-# Methods defined in the helpers block are available in templates
-# helpers do
-#   def some_helper
-#     "Helping"
-#   end
-# end
-
 set :css_dir, 'css'
 
 set :js_dir, 'js'
 
 set :images_dir, 'img'
 
-# Build-specific configuration
-configure :build do
-  # For example, change the Compass output style for deployment
-  # activate :minify_css
-
-  # Minify Javascript on build
-  # activate :minify_javascript
-
-  # Enable cache buster
-  # activate :asset_hash
-
-  # Use relative URLs
-  # activate :relative_assets
-
-  # Or use a different image path
-  # set :http_prefix, "/Content/images/"
+with_layout :dress do
+  page "/wedding-dresses/*"
 end
 
 activate :blog do |blog|
@@ -74,7 +13,6 @@ activate :blog do |blog|
   blog.permalink = ':year/:month/:day/:title.html'
   blog.sources = ':year-:month-:day-:title.html'
   blog.taglink = 'tags/:tag.html'
-  # blog.layout = 'blog'
   blog.default_extension = ".markdown"
 
   blog.tag_template = "tag.html"
@@ -98,5 +36,7 @@ helpers DressHelpers
 helpers HomeHelpers
 
 ready do
+  # we have to inject the sitemap because it isn't initialised until
+  # after the dresses have been created
   dresses.each{|dress| dress.sitemap = sitemap }
 end
